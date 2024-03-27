1503e40120b627900866653529c21cca8d3e874f
esra <esrabaskaya84@gmail.com>
2024-03-27 16:41:45 +0100

    register

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+	return <div>Coming Soon...</div>;
+};
+
+export default Register;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import About from "../pages/about/About";
 import Details from "../pages/details/Details";
+import Register from "../pages/register/Register";
 import PrivateRouter from "./PrivateRouter";
 import NotFound from "../pages/errorPage/NotFound";
 
@@ -18,11 +19,13 @@ const AppRouter = () => {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
 					<Route path="/about" element={<PrivateRouter />}>
 						<Route path="" element={<About />} />
 					</Route>
 					<Route path="/details" element={<PrivateRouter />}>
 						<Route path="" element={<Details />} />
+
 						<Route path="*" element={<NotFound />} />
 					</Route>
 				</Routes>
